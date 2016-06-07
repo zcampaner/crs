4e4d560889a2f9c3c71f6a6793643a5fe7bb3a95
zcampaner <zcampaner@gmail.com>
2016-06-07 15:17:50 +0800

    added index.php and other library source files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Central Reservation System</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css.map">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	<script type="text/javascript" src="angular.min.css"></script>
+</head>
+<body>
+	
+</body>
+</html>
